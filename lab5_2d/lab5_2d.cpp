7d9638ac1a8eb99cd35915de673d753e9c2a01fe
Newstar12321 <tazeev.zhenya@mail.ru>
2023-11-20 11:37:22 +0700

    Добавьте файлы проекта.

---
+++ lab5_2d/lab5_2d.cpp
@@ -0,0 +1,68 @@
+﻿/*
+2. Дан массив размера N.
+Возвести в квадрат все его локальные минимумы
+(то есть числа, меньшие своих соседей).
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	size_t n;
+	cout << "Введите число элементов массива:";
+	cin >> n;
+	if (n == 0)
+	{
+		cerr << "Ошибка: размер массива не может быть равен 0";
+		return 1;
+	}
+
+	int* array;
+	array = new int[n];
+
+	cout << "Введите элементы массива:" << endl;
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << i << ") ";
+		cin >> array[i];
+	}
+
+	int* new_array;
+	new_array = new int[n];
+
+	new_array[0] = array[0];
+	if (array[0] < array[1])
+	{
+		new_array[0] *= new_array[0];
+	}
+
+	for (size_t i = 1; i < (n - 1); i++)
+	{
+		int a0 = array[i - 1], a1 = array[i], a2 = array[i + 1];
+		new_array[i] = a1;
+		if ((a1 < a0) && (a1 < a2))
+		{
+			new_array[i] *= new_array[i];
+		}
+	}
+
+	new_array[n - 1] = array[n - 1];
+	if (array[n - 1] < array[n - 2])
+	{
+		new_array[n - 1] *= new_array[n - 1];
+	}
+
+	cout << "[" << new_array[0];
+	for (size_t i = 1; i < n; i++)
+	{
+		cout << ", " << new_array[i];
+	}
+	cout << "]";
+
+	delete[] array;
+	delete[] new_array;
+}
